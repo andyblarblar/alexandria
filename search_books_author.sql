46431014ed199add1f9f1dc2714b3189d50de155
Jimmy Samson <82990445+jsamson23@users.noreply.github.com>
2023-03-23 15:43:36 -0400

    Update search_books_author.sql

--- search_books_author.sql
+++ search_books_author.sql
@@ -1,5 +1,5 @@
 -- Views the libraries and locations that have books available on a given book author
-SELECT L.lib_id, name, location
+SELECT L.lib_id, name, location, title, genre, BOOKS.call_num
 FROM BOOKS
     JOIN LIBRARIES L on L.lib_ID = BOOKS.lib_id
 WHERE author == {} AND NOT EXISTS(SELECT * FROM CHECKS_OUT AS CO WHERE CO.b_lib_id == L.lib_id AND CO.call_num = BOOKS.call_num)
