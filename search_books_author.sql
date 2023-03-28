2c42e893993a1b577ffa2c272e654c387b98192d
Andrew Ealovega <Andrew@Ealovega.dev>
2023-03-28 16:51:26 -0400

    Make searches work

--- search_books_author.sql
+++ search_books_author.sql
@@ -1,6 +1,6 @@
 -- Views the libraries and locations that have books available on a given book author
-SELECT L.lib_id, name, location, title, genre, BOOKS.call_num
+SELECT L.lib_id, name, location, title, genre, BOOKS.author, BOOKS.call_num
 FROM BOOKS
          JOIN LIBRARIES L on L.lib_ID = BOOKS.lib_id
-WHERE author LIKE ?
+WHERE author LIKE '%' || ? || '%'
   AND NOT EXISTS(SELECT * FROM CHECKS_OUT AS CO WHERE CO.b_lib_id == L.lib_id AND CO.call_num = BOOKS.call_num)
--- search_books_title.sql
+++ search_books_title.sql
@@ -2,5 +2,5 @@
 SELECT L.lib_id, name, location, title, author, genre, BOOKS.call_num
 FROM BOOKS
          JOIN LIBRARIES L on L.lib_ID = BOOKS.lib_id
-WHERE title LIKE ?
+WHERE title LIKE '%' || ? || '%'
   AND NOT EXISTS(SELECT * FROM CHECKS_OUT AS CO WHERE CO.b_lib_id == L.lib_id AND CO.call_num = BOOKS.call_num)
