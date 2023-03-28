f56ca472bf6ae206e5c2c765b0d1d5fb84898455
Jimmy Samson <82990445+jsamson23@users.noreply.github.com>
2023-03-27 21:19:51 -0400

    Update search_books_genre.sql

--- search_books_genre.sql
+++ search_books_genre.sql
@@ -2,5 +2,5 @@
 SELECT L.lib_id, name, location, title, author, BOOKS.call_num
 FROM BOOKS
          JOIN LIBRARIES L on L.lib_ID = BOOKS.lib_id
-WHERE genre == ?
+WHERE genre LIKE ?
   AND NOT EXISTS(SELECT * FROM CHECKS_OUT AS CO WHERE CO.b_lib_id == L.lib_id AND CO.call_num = BOOKS.call_num)
